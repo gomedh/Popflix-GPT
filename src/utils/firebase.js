521697c086f91f9eb19ada7a8ed59008b5ade04e
gomedh <gomedh.tak1@gmail.com>
2024-05-18 11:49:36 +0100

    Added Firebase for authentication and hosting

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB58-3En1pe6nWzT7mnPQuKiKx_k1FsOzk",
+  authDomain: "netflixgpt04.firebaseapp.com",
+  projectId: "netflixgpt04",
+  storageBucket: "netflixgpt04.appspot.com",
+  messagingSenderId: "673108042710",
+  appId: "1:673108042710:web:9d04e10d457b235a4c3a77",
+  measurementId: "G-N19YPTZ3JJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
